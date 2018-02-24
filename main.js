f23a8bf6ae75ac1fc58cfe11d796433b26ef30f5
David Bruant <bruant.d@gmail.com>
2018-02-24 12:04:37 -0500

    Successfully turned an issue into a PR Fixes #3

--- main.js
+++ main.js
@@ -1,55 +1,8 @@
-throw 'TODO : apparently, to create a pull request, there needs to be additional commits...blaaaahhhhh';
-throw 'missing token';
+'use strict';
 
-var BASE_GITHUB_API = 'https://api.github.com'
+throw 'missing token'
 
-// URLs
-function makeCommitURL(owner, repo){
-    return BASE_GITHUB_API+'/repos/'+owner+'/'+repo+'/commits';
-}
-
-function makePullRequestURL(owner, repo){
-    return BASE_GITHUB_API+'/repos/'+owner+'/'+repo+'/pulls';
-}
-
-function makeRefURL(owner, repo){
-    return BASE_GITHUB_API+'/repos/'+owner+'/'+repo+'/git/refs';
-}
-
-
-// API functions
-function getLatestCommitSha(owner, repo){
-    const response = UrlFetchApp.fetch(
-        makeCommitURL(owner, repo)+'?access_token='+token
-    );
-
-    return JSON.parse(response.getContentText())[0].sha
-}
-
-
-function makeBranchOverMainBranch(owner, repo, newBranchName){
-    const sha = getLatestCommitSha(owner, repo);
-
-    const resp = UrlFetchApp.fetch(
-        makeRefURL(owner, repo)+'?access_token='+token, 
-        {
-            'method' : 'post',
-            'payload' : JSON.stringify({
-                'ref': 'refs/heads/'+newBranchName,
-                'sha': sha
-            }),
-            'muteHttpExceptions': true
-        }
-    );
-}
-
-
-
-// Business
-function turnIssueIntoPR(owner, repo, issueId){
-    const PRhead = "issue-"+issueId;
-
-    makeBranchOverMainBranch(owner, repo, PRhead);
+function turnIssueIntoPR(owner, repo, issueId, branch){
 
     const resp2 = UrlFetchApp.fetch(
         makePullRequestURL(owner, repo)+'?access_token='+token, 
@@ -57,7 +10,7 @@ function turnIssueIntoPR(owner, repo, issueId){
             'method' : 'post',
             'payload' : JSON.stringify({
                 "issue": issueId,
-                "head": PRhead,
+                "head": branch,
                 "base": "master"
             }),
             'muteHttpExceptions': true
@@ -68,5 +21,5 @@ function turnIssueIntoPR(owner, repo, issueId){
 }
 
 function test(){
-    turnIssueIntoPR('DavidBruant', 'turn-issues-into-prs', '1')
+    turnIssueIntoPR('DavidBruant', 'turn-issues-into-prs', '1', 'test-2')
 }
